ef499a87412616b5cd221e50d388cb4835d12d3d
m <m@avalon-asics.com>
2013-08-24 15:13:08 +0800

    verilog: extract DCM into xilinx/clkgen.v
    Signed-off-by: m <m@avalon-asics.com>

--- verilog/main.v
+++ verilog/main.v
@@ -20,29 +20,7 @@ module mm(
 );
 
 wire clk_kdf9, reset_kdf9_n;
-DCM_CLKGEN #( // {{{
-	.CLKFXDV_DIVIDE(2), // CLKFXDV divide value (2, 4, 8, 16, 32)
-	.CLKFX_DIVIDE(16), // Divide value - D - (1-256)
-	.CLKFX_MD_MAX(0.0), // Specify maximum M/D ratio for timing anlysis
-	.CLKFX_MULTIPLY(50), // Multiply value - M - (2-256)
-	.CLKIN_PERIOD(30.0), // Input clock period specified in nS
-	.SPREAD_SPECTRUM("NONE"), // Spread Spectrum mode "NONE", "CENTER_LOW_SPREAD", "CENTER_HIGH_SPREAD",
-	// "VIDEO_LINK_M0", "VIDEO_LINK_M1" or "VIDEO_LINK_M2"
-	.STARTUP_WAIT("FALSE") // Delay config DONE until DCM_CLKGEN LOCKED (TRUE/FALSE)
-) DCM_CLKGEN_kdf9 (
-	.CLKFX(clk_kdf9), // 1-bit output: Generated clock output
-	.CLKFX180(), // 1-bit output: Generated clock output 180 degree out of phase from CLKFX.
-	.CLKFXDV(), // 1-bit output: Divided clock output
-	.LOCKED(reset_kdf9_n), // 1-bit output: Locked output
-	.PROGDONE(), // 1-bit output: Active high output to indicate the successful re-programming
-	.STATUS(), // 2-bit output: DCM_CLKGEN status
-	.CLKIN(CLK), // 1-bit input: Input clock
-	.FREEZEDCM(1'b0), // 1-bit input: Prevents frequency adjustments to input clock
-	.PROGCLK(1'b0), // 1-bit input: Clock input for M/D reconfiguration
-	.PROGDATA(1'b0), // 1-bit input: Serial data input for M/D reconfiguration
-	.PROGEN(1'b0), // 1-bit input: Active high program enable
-	.RST(1'b0) // 1-bit input: Reset input pin
-); // }}}
+clkgen clk (.clkin(CLK), .clkout(clk_kdf9), .locked(reset_kdf9_n));
 
 wire [31:0] interrupt_n;
 wire [31:0] user_operand_0, user_operand_1, user_result;
