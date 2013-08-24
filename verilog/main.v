ba17b0d73c4d04858c44948f4fed1c68d167a748
m <m@avalon-asics.com>
2013-08-24 14:52:43 +0800

    verilog: add intr. testing logic
    Signed-off-by: m <m@avalon-asics.com>

--- verilog/main.v
+++ verilog/main.v
@@ -44,21 +44,67 @@ DCM_CLKGEN #( // {{{
 	.RST(1'b0) // 1-bit input: Reset input pin
 ); // }}}
 
-wire[31:0] interrupt_n;
-superkdf9 cpu (.clk(clk_kdf9), .rst(~reset_kdf9_n) /*, .interrupt_n(interrupt_n)*/);
+wire [31:0] interrupt_n;
+wire [31:0] user_operand_0, user_operand_1, user_result;
+wire [10:0] user_opcode;
+wire user_valid, user_complete;
+superkdf9 cpu ( // {{{
+	.clk(clk_kdf9), .rst(~reset_kdf9_n), 
+	// active low interrups
+	.interrupt_n(interrupt_n),
+	// user-defined instruction interface
+	.user_opcode(user_opcode),
+	.user_operand_0(user_operand_0),
+	.user_operand_1(user_operand_1),
+	.user_result(user_result),
+	.user_valid(user_valid),
+	.user_complete(user_complete)
+); // }}}
 //assign LED = gpio_out[3:0];
 assign {FLASH_MOSI, FLASH_SCK, FLASH_NCS, TXD} = 4'b0011;
 
-reg[31:0] count;
-reg led;
-assign LED = {4{led}};
+// test user instructions
+//reg user_complete_reg;
+//assign user_complete = user_complete_reg;
+//always @(posedge clk_kdf9) user_complete_reg <= user_valid;
+assign user_complete = user_valid;
+assign user_result = user_operand_0 + user_operand_1;
+
+reg [3:0] led_reg;
+assign LED = led_reg;
 always @(posedge clk_kdf9) begin
-	count <= count + 1;
-	if (count == 32'd50_000_000) begin
-		count <= 0;
-		led <= ~led;
+	if (user_valid && user_opcode == 11'h1)
+		led_reg <= user_operand_0[3:0];
+end
+
+// test intr logic
+reg [31:0] intr, intr_i_old;
+assign interrupt_n = intr;
+wire [31:0] intr_i;
+always @(posedge clk_kdf9) begin
+	if (rst) begin
+		intr <= 32'hffff_ffff;
+		intr_i_old <= intr_i;
+	end else begin
+		intr_i_old <= intr_i;
+		if (intr_i != intr_i_old) begin
+			intr <= intr_i;
+		end
+		if (user_opcode == 11'hf && user_valid) begin
+			intr <= 32'hfff_ffff;
+		end
 	end
 end
+
+// VIO/ILA and ICON {{{
+wire [35:0] icon_ctrl_0, icon_ctrl_1;
+icon icon_test(.CONTROL0(icon_ctrl_0), .CONTROL1(icon_ctrl_1));
+ila ila_test(.CONTROL(icon_ctrl_0), .CLK(clk_kdf9), .TRIG0({
+	rst, user_complete, user_valid, user_opcode[4:0]
+	,user_operand_0[31:0]
+}));
+vio vio_test(.CONTROL(icon_ctrl_1), .ASYNC_OUT(intr_i));
+// }}}
 
 endmodule
 // vim: set fdm=marker : 
