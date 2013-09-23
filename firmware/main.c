4f023bf7385c5a167203a6e6e37721e1b941e64d
Xiangfu <xiangfu@openmobilefree.net>
2013-09-23 17:38:53 +0800

    * Cleanup the code style. please use Linux kernel code stype * Remove all comment code. don't commit any comment-code. just remove them * Cleanup the whitespace.

--- firmware/main.c
+++ firmware/main.c
@@ -1,36 +1,35 @@
+//
+// Author: Minux
+// Author: Xiangfu Liu <xiangfu@openmobilefree.net>
+// Bitcoin:	1CanaaniJzgps8EV6Sfmpb7T8RutpaeyFn
+//
+// This is free and unencumbered software released into the public domain.
+// For details see the UNLICENSE file at the root of the source tree.
+//
+
 #include "sdk.h"
 #include "minilibc.h"
 
-void
-setled(uint32_t led) {
+void setled(uint32_t led) {
 	int t;
 	asm volatile ("user %0, %1, %1, 0x1": "=r"(t) : "r"(led));
 }
 
-void
-delay(volatile uint32_t i) {
+void delay(volatile uint32_t i) {
 	while (i--)
 		;
 }
 
-int
-main(void) {
+int main(void) {
 	int i = 0, j;
-	unsigned int *gpio_pio_data = (unsigned int *)0x80000200 ; 
-	unsigned int *uart0_buf = ( unsigned int *)0x80000100 ; 
-	//unsigned int *gpio_pio_tri  = (unsigned int *)0x80000204 ; 
-	//irq_set_mask(0x1);
-	//irq_enable();
+	unsigned int *gpio_pio_data = (unsigned int *)0x80000200;
+	unsigned int *uart0_buf = (unsigned int *)0x80000100;
 	j = 1;
 	while (1) {
-		//setled(j);
 		delay(16000000);
 		j++;
 		*gpio_pio_data = 0x00345678|(j<<24) ;
 		*uart0_buf = 0x12345678 ;
-		//j <<= 1;
-		//if (j > (1 << 3))
-		//	j = 1;
 	}
 	while (1) {
 		asm volatile ("rcsr    %0, IP": "=r"(i));
@@ -48,4 +47,4 @@ main(void) {
 	return(0);
 }
 
-// vim: set ts=4 sw=4 fdm=marker : 
+// vim: set ts=4 sw=4 fdm=marker :
