3e1dfed5a681b7d56132ecce09196603c6b30819
Xiangfu <xiangfu@openmobilefree.net>
2013-09-25 11:04:34 +0800

    add load rule (using the ise impact)

--- firmware/main.c
+++ firmware/main.c
@@ -12,15 +12,42 @@
 
 #include "system_config.h"
 
-static void set_led(uint32_t led) {
+static void set_led(uint32_t led)
+{
 	volatile uint32_t *gpio_pio_data = (uint32_t *)GPIO_BASE;
 
 	*gpio_pio_data = led;
 }
 
-static void delay(volatile uint32_t i) {
+static void delay(volatile uint32_t i)
+{
 	while (i--)
 		;
+}
+
+static inline int reg_read(unsigned int reg)
+{
+	return *((int*)(reg));
+}
+
+static inline void reg_write(unsigned int reg, int value)
+{
+	*((int*)(reg)) = value;
+}
+
+void uart_init(struct lm32_uart *uart, int baud)
+{
+	/* Disable UART interrupts */
+	reg_write(uart->ier, 0);
+
+	/* Line control 8 bit, 1 stop, no parity */
+	reg_write(uart->lcr, LM32_UART_LCR_8BIT);
+
+	/* Modem control, DTR = 1, RTS = 1 */
+	reg_write(uart->mcr, LM32_UART_MCR_DTR | LM32_UART_MCR_RTS);
+
+	/* Set baud rate */
+	reg_write(uart->div, CPU_FREQUENCY / baud);
 }
 
 int main(void) {
