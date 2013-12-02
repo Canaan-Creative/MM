15404513ffaa03ffa0063108a3c2218e34c47ef6
Jingjie <jingjie@canaan-creative.com>
2013-12-02 04:51:50 -0500

    change cnt to 26bit

--- verilog/superkdf9/components/alink/tx_timer.v
+++ verilog/superkdf9/components/alink/tx_timer.v
@@ -10,17 +10,17 @@ output reg        timer_busy   ,
 output [31:0]     timer_cnt       
 );
 
-reg [23:0] timer ;
+reg [25:0] timer ;
 
-assign timer_cnt = {8'b0,timer} ;
+assign timer_cnt = {6'b0,timer} ;
 
 always @ ( posedge clk ) begin
 	if( rst || (timer == reg_tout) || reg_flush )
-		timer <= 24'b0 ;
+		timer <= 'b0 ;
 	else if( timer_start )
-		timer <= 24'b1 ;
+		timer <= 'b1 ;
 	else if( |timer && (timer < reg_tout))
-		timer <= 24'b1 + timer ;
+		timer <= 'b1 + timer ;
 end
 
 always @ ( posedge clk ) begin
@@ -28,7 +28,7 @@ always @ ( posedge clk ) begin
 		timer_busy <= 1'b0 ;
 	else if( timer_start )
 		timer_busy <= 1'b1 ;
-	else if ( (timer == reg_tout[23:0]) && |reg_tout[23:0] )
+	else if ( (timer == reg_tout[25:0]) && |reg_tout[25:0] )
 		timer_busy <= 1'b0 ;
 end
 endmodule
