967cf385682123729514e9d95e09fcd9b6428bd3
Xiangfu <xiangfu@openmobilefree.net>
2014-03-10 21:39:33 +0800

    add Avalon3 support

--- firmware/system_config.h
+++ firmware/system_config.h
@@ -13,7 +13,7 @@
 #define AVA2_DEFAULT_MINERS	10
 
 #define ASIC_FREQUENCY	1500
-#define ASIC_COUNT	7
+#define ASIC_COUNT	5
 
 #define CPU_FREQUENCY		(100 * 1000 * 1000) /* 50Mhz */
 #define UART_BAUD_RATE          (115200)
