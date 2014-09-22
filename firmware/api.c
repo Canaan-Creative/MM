87cd097255e68637f9fe074d22ea1550177937c0
jingjie <jingjie@canaan-creative.com>
2014-09-22 16:02:18 +0800

    Add api.c (avalon testing code and led control)

---
+++ firmware/api.c
@@ -0,0 +1,151 @@
+unsigned int api_gen_test_work(unsigned int i, unsigned int chip_under_test_num, unsigned int * data){
+	unsigned int tmp = 0;
+        if(i%16 == 0 ){data[0]=0x1bed3ba0; data[1]=0xa2cb45c1; data[2]=0xd8f8ef67; data[3]=0x12146495; data[4]=0xc44192c0; data[5]=0x7145fd6d; data[6]=0x974bf4bb; data[7]=0x8f41371d; data[8]=0x65c90d1e; data[9]=0x9cb18a17; data[10]=0xfa77fe7d; data[11]=0x13cdfd7b; data[12]=0x00639107; data[13]=0x62a5f25c; data[14]=0x06b168ae; data[15]=0x087e051a; data[16]=0x89517050; data[17]=0x4ac1d001;}
+        if(i%16 == 1 ){data[0]=0xc1680161; data[1]=0x9d8d4242; data[2]=0xe06e5fab; data[3]=0x25a54bbe; data[4]=0x222e8b87; data[5]=0x7848c34b; data[6]=0xeea79cd6; data[7]=0x528caf7e; data[8]=0x33cde02a; data[9]=0x983dab15; data[10]=0x8119ce2a; data[11]=0x1c9fc4ed; data[12]=0xdac8ce29; data[13]=0x6d0fd9da; data[14]=0x6e18f645; data[15]=0x087e051a; data[16]=0x2d547050; data[17]=0xe8dc86b1;}
+        if(i%16 == 2 ){data[0]=0x84a9e522; data[1]=0xb6327d35; data[2]=0x2a54a40c; data[3]=0x879a9334; data[4]=0xd962d729; data[5]=0x5600f2af; data[6]=0xf81e2a4b; data[7]=0x80a27591; data[8]=0x4064bca0; data[9]=0x612fc425; data[10]=0xa1d03421; data[11]=0xf8941ad5; data[12]=0xc5e7acf1; data[13]=0xb0496c6a; data[14]=0xc35604f4; data[15]=0x087e051a; data[16]=0x3c537050; data[17]=0x826cce7a;}
+        if(i%16 == 3 ){data[0]=0x1c11ef93; data[1]=0xb3baed4e; data[2]=0x2a54a40c; data[3]=0x879a9334; data[4]=0xd962d729; data[5]=0x5600f2af; data[6]=0xf81e2a4b; data[7]=0x80a27591; data[8]=0x4064bca0; data[9]=0x612fc425; data[10]=0xa1d03421; data[11]=0xfa941ad5; data[12]=0xc3efecf2; data[13]=0xb0496c6a; data[14]=0xc55604f4; data[15]=0x087e051a; data[16]=0x3e537050; data[17]=0x826cce7a;}
+        if(i%16 == 4 ){data[0]=0xad258354; data[1]=0xcb517d22; data[2]=0x81c9ffff; data[3]=0xdd3da630; data[4]=0xea65fc9d; data[5]=0x857ccb58; data[6]=0x8a776518; data[7]=0x67eb9370; data[8]=0xed14a977; data[9]=0xf1d2de33; data[10]=0xa1235784; data[11]=0x65d344fe; data[12]=0xc6e2feca; data[13]=0x431284bc; data[14]=0x5e2e867c; data[15]=0x087e051a; data[16]=0x70527050; data[17]=0xc290e5a7;}
+        if(i%16 == 5 ){data[0]=0xdaa34ad5; data[1]=0xb171b59c; data[2]=0xd8f8ef67; data[3]=0x12146495; data[4]=0xc44192c0; data[5]=0x7145fd6d; data[6]=0x974bf4bb; data[7]=0x8f41371d; data[8]=0x65c90d1e; data[9]=0x9cb18a17; data[10]=0xfa77fe7d; data[11]=0x19cdfd7b; data[12]=0x068a510a; data[13]=0x62a5f25c; data[14]=0x0cb168ae; data[15]=0x087e051a; data[16]=0x8f517050; data[17]=0x4ac1d001;}
+        if(i%16 == 6 ){data[0]=0x010dceb6; data[1]=0x220f1dbd; data[2]=0xd8f8ef67; data[3]=0x12146495; data[4]=0xc44192c0; data[5]=0x7145fd6d; data[6]=0x974bf4bb; data[7]=0x8f41371d; data[8]=0x65c90d1e; data[9]=0x9cb18a17; data[10]=0xfa77fe7d; data[11]=0x12cdfd7b; data[12]=0x81677107; data[13]=0x62a5f25c; data[14]=0x05b168ae; data[15]=0x087e051a; data[16]=0x88517050; data[17]=0x4ac1d001;}
+        if(i%16 == 7 ){data[0]=0x2bc36997; data[1]=0xb1231d47; data[2]=0x2a54a40c; data[3]=0x879a9334; data[4]=0xd962d729; data[5]=0x5600f2af; data[6]=0xf81e2a4b; data[7]=0x80a27591; data[8]=0x4064bca0; data[9]=0x612fc425; data[10]=0xa1d03421; data[11]=0xfc941ad5; data[12]=0xc1d82cf3; data[13]=0xb0496c6a; data[14]=0xc75604f4; data[15]=0x087e051a; data[16]=0x40537050; data[17]=0x826cce7a;}
+        if(i%16 == 8 ){data[0]=0x62af5f08; data[1]=0x312bf034; data[2]=0x740d56be; data[3]=0xe2e8080f; data[4]=0x19fae6f7; data[5]=0xc35ad8db; data[6]=0x42727a8e; data[7]=0xff62389c; data[8]=0xc572d901; data[9]=0xcc39201c; data[10]=0x92f7791a; data[11]=0x24d6e2cd; data[12]=0x67d1e0c0; data[13]=0xf55f91a6; data[14]=0x39171075; data[15]=0x087e051a; data[16]=0x13557050; data[17]=0x9ed94986;}
+        if(i%16 == 9 ){data[0]=0x2068a649; data[1]=0xb0cc4d3c; data[2]=0x2a54a40c; data[3]=0x879a9334; data[4]=0xd962d729; data[5]=0x5600f2af; data[6]=0xf81e2a4b; data[7]=0x80a27591; data[8]=0x4064bca0; data[9]=0x612fc425; data[10]=0xa1d03421; data[11]=0xf6941ad5; data[12]=0xc0016cf0; data[13]=0xb0496c6a; data[14]=0xc15604f4; data[15]=0x087e051a; data[16]=0x3a537050; data[17]=0x826cce7a;}
+        if(i%16 == 10){data[0]=0xc95c48ca; data[1]=0x26fe7d8b; data[2]=0xd8f8ef67; data[3]=0x12146495; data[4]=0xc44192c0; data[5]=0x7145fd6d; data[6]=0x974bf4bb; data[7]=0x8f41371d; data[8]=0x65c90d1e; data[9]=0x9cb18a17; data[10]=0xfa77fe7d; data[11]=0x16cdfd7b; data[12]=0x8156f105; data[13]=0x62a5f25c; data[14]=0x09b168ae; data[15]=0x087e051a; data[16]=0x8c517050; data[17]=0x4ac1d001;}
+        if(i%16 == 11){data[0]=0x1033995b; data[1]=0x9a7ce254; data[2]=0xe06e5fab; data[3]=0x25a54bbe; data[4]=0x222e8b87; data[5]=0x7848c34b; data[6]=0xeea79cd6; data[7]=0x528caf7e; data[8]=0x33cde02a; data[9]=0x983dab15; data[10]=0x8119ce2a; data[11]=0x189fc4ed; data[12]=0xdab84e2b; data[13]=0x6d0fd9da; data[14]=0x6a18f645; data[15]=0x087e051a; data[16]=0x29547050; data[17]=0xe8dc86b1;}
+        if(i%16 == 12){data[0]=0x93753bfc; data[1]=0x2a6249eb; data[2]=0xe06e5fab; data[3]=0x25a54bbe; data[4]=0x222e8b87; data[5]=0x7848c34b; data[6]=0xeea79cd6; data[7]=0x528caf7e; data[8]=0x33cde02a; data[9]=0x983dab15; data[10]=0x8119ce2a; data[11]=0x239fc4ed; data[12]=0x545dae3e; data[13]=0x6d0fd9da; data[14]=0x7518f645; data[15]=0x087e051a; data[16]=0x34547050; data[17]=0xe8dc86b1;}
+        if(i%16 == 13){data[0]=0x9ea1e81d; data[1]=0xa82e81f6; data[2]=0xe06e5fab; data[3]=0x25a54bbe; data[4]=0x222e8b87; data[5]=0x7848c34b; data[6]=0xeea79cd6; data[7]=0x528caf7e; data[8]=0x33cde02a; data[9]=0x983dab15; data[10]=0x8119ce2a; data[11]=0x249fc4ed; data[12]=0xd369ce3d; data[13]=0x6d0fd9da; data[14]=0x7618f645; data[15]=0x087e051a; data[16]=0x35547050; data[17]=0xe8dc86b1;}
+        if(i%16 == 14){data[0]=0x873c2a5e; data[1]=0x2d1a8543; data[2]=0x81c9ffff; data[3]=0xdd3da630; data[4]=0xea65fc9d; data[5]=0x857ccb58; data[6]=0x8a776518; data[7]=0x67eb9370; data[8]=0xed14a977; data[9]=0xf1d2de33; data[10]=0xa1235784; data[11]=0x5ad344fe; data[12]=0x29ebded7; data[13]=0x431284bc; data[14]=0x532e867c; data[15]=0x087e051a; data[16]=0x65527050; data[17]=0xc290e5a7;}
+        if(i%16 == 15){data[0]=0xa2bef63f; data[1]=0x30101538; data[2]=0x2a54a40c; data[3]=0x879a9334; data[4]=0xd962d729; data[5]=0x5600f2af; data[6]=0xf81e2a4b; data[7]=0x80a27591; data[8]=0x4064bca0; data[9]=0x612fc425; data[10]=0xa1d03421; data[11]=0xf7941ad5; data[12]=0x41054cf0; data[13]=0xb0496c6a; data[14]=0xc25604f4; data[15]=0x087e051a; data[16]=0x3b537050; data[17]=0x826cce7a;}
+	tmp = data[0];
+        data[0] = data[0] ^ (i & 0xfffffff0);//nonce
+
+        data[2] = data[2] - chip_under_test_num;//role time
+
+        data[18] = 0x0;//nonce2
+        data[19] = i;//nonce2
+
+        data[20] = 0x1;//cpm2
+        data[21] = 0x1;//cpm1
+        data[22] = 0x1;//cpm0
+
+	return tmp + 0x18000;
+}
+
+void api_set_num(unsigned int ch_num, unsigned int chip_num){
+	unsigned int tmp;
+	tmp = readl(0x80000510) & 0xff;//reg sck
+	tmp = (ch_num << 16) | (chip_num << 24) | tmp;
+	writel(tmp, 0x80000510);
+}
+
+void api_set_sck(unsigned int spi_speed){
+	unsigned int tmp;
+	tmp = (readl(0x80000510) & 0xffff0000) | spi_speed;
+	writel(tmp, 0x80000510);
+}
+
+void api_set_timeout(unsigned int timeout){
+	writel(timeout, 0x8000050c);
+}
+
+void api_set_flush(){
+	writel(0x2, 0x80000508);
+}
+
+void api_initial(unsigned int ch_num, unsigned int chip_num, unsigned int spi_speed, unsigned int timeout){
+	api_set_num(ch_num, chip_num);
+	api_set_sck(spi_speed);
+	api_set_flush();
+}
+
+unsigned int api_get_tx_cnt(){
+	unsigned int tmp = (readl(0x80000508) >> 2) && 0x3ff;
+	return tmp;
+}
+
+unsigned int api_get_rx_cnt(){
+	unsigned int tmp = (readl(0x80000508) >> 20) && 0x1ff;
+	return tmp;
+}
+
+void api_set_tx_fifo(unsigned int * data){
+	int i;
+	for(i = 0; i < 23; i++){
+		writel(data[i], 0x80000500);
+	}
+}
+
+void api_get_rx_fifo(unsigned int * data){
+	int i;
+	for(i = 0; i < 4; i++){
+		data[i] = readl(0x80000504);
+	}
+}
+
+void api_wait_done(unsigned int ch_num, unsigned int chip_num){
+	while(api_get_rx_cnt() != (ch_num * chip_num * 4));
+}
+
+unsigned int api_verify_nonce(
+unsigned int ch_num,
+unsigned int chip_num,
+unsigned int chip_under_test_num,
+unsigned int verify_on,
+unsigned int target_nonce
+){
+	unsigned int i, j, need_verify;
+	unsigned int rx_data[4];
+	unsigned int pass_cal_num = 0;
+	for(i = 0; i < ch_num; i++){
+		for(j = 0; j < chip_num; j++){
+			api_get_rx_fifo(rx_data);
+			need_verify = ((chip_num - chip_under_test_num - 1) == j) ? 1 : 0;
+			if(verify_on && (rx_data[2] == target_nonce) && need_verify)
+				pass_cal_num++;
+		}
+	}
+	return pass_cal_num;
+}
+
+unsigned int api_asic_test(
+unsigned int ch_num, 
+unsigned int chip_num,
+unsigned int cal_core_num
+){
+	unsigned int i, j, k;
+	unsigned int tx_data[23];
+	unsigned int target_nonce;
+	unsigned int pass_cal_num = 0;
+	unsigned int verify_on = 0;
+	unsigned int spi_speed = 0x10;
+	unsigned int timeout = 0x5000;
+	api_initial(ch_num, chip_num, spi_speed, timeout);
+
+	for(i = 0; i < chip_num; i++){
+		for(j = 0; j < cal_core_num + 2; j++){
+			api_gen_test_work(j, i, tx_data);
+			for(k = 0; k < ch_num; k++)
+				api_set_tx_fifo(tx_data);
+
+			api_wait_done(ch_num, chip_num);
+			target_nonce = api_gen_test_work((j-2)%16, 0, tx_data);
+			verify_on = j >= 2 ? 1 : 0;
+			pass_cal_num += api_verify_nonce(ch_num, chip_num, i, verify_on, target_nonce);
+		}
+	}
+	return pass_cal_num;
+}
+
+void sft_led(unsigned char data){
+	int i;
+	unsigned int tmp = 0;
+	tmp = readl(0x80000624) & 0xfffffffc;
+	for(i = 0; i < 8; i++){
+		writel((tmp & 0xfffffffc) | (data & 1)    , 0x80000624);//clk low
+
+		writel((tmp & 0xfffffffc) | (data & 1) | 2, 0x80000624);//clk high
+
+		writel((tmp & 0xfffffffc) | (data & 1)    , 0x80000624);//clk low
+		data = data >> 1;
+	}
+}
