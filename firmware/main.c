0bdbdce6a1dad06afe45fdf678aa029ad1f29904
Xiangfu <xiangfu@openmobilefree.net>
2014-09-23 21:06:27 +0800

    Fix typo

--- firmware/main.c
+++ firmware/main.c
@@ -322,9 +322,9 @@ static int read_result(struct mm_work *mw, struct result *ret)
 			data = ret_buf[ret_produce];
 			ret_produce = (ret_produce + 1) & RET_RINGBUFFER_MASK_RX;
 			uint32_t tmp;
-			memcpy(&tmp, (uint8_t *)&nonce0, 4);
+			memcpy(&tmp, ret->nonce, 4);
 			tmp = tmp - 0x4000 + 0x180;
-			memcpy((uint8_t *)&nonce0, &tmp, 4);
+			memcpy(ret->nonce, &tmp, 4);
 			memcpy(data, (uint8_t *)ret, 20);
 			memcpy(data + 20, mw->job_id, 4); /* Attach the job_id */
 		}
