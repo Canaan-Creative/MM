debaac8a24ba62827832628315a85225d893d018
jingjie <jingjie@canaan-creative.com>
2014-10-17 16:07:28 +0800

    add led

---
+++ verilog/superkdf9/components/twi/led_shift.v
@@ -0,0 +1,42 @@
+module led_shift(
+input        clk      ,
+input        rst      ,
+input        vld      ,
+input  [7:0] din      ,
+output       done     ,
+
+output       sft_shcp ,
+output       sft_ds    
+);
+
+//--------------------------------------------------
+// shcp counter
+//--------------------------------------------------
+reg [5:0] shcp_cnt ;
+always @ ( posedge clk ) begin
+	if( rst )
+		shcp_cnt <= 0 ;
+	else if( vld )
+		shcp_cnt <= 1 ;
+	else if( |shcp_cnt )
+		shcp_cnt <= shcp_cnt + 1 ;
+end
+
+assign sft_shcp = shcp_cnt[2] ;
+
+reg [7:0] data ;
+always @ ( posedge clk ) begin
+	if( vld )
+		data <= din ;
+	else if( &shcp_cnt[2:0] )
+		data <= data >> 1 ;
+end
+
+assign sft_ds = vld ? din[0] : data[0] ;
+
+//--------------------------------------------------
+// done
+//--------------------------------------------------
+assign done = shcp_cnt == 63 ;
+
+endmodule
