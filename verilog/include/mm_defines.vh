8cd81e28428eb33d7a5144421cfc1c8c1bde4a7b
jingjie <jingjie@canaan-creative.com>
2014-12-27 11:37:58 +0800

    change 100 to 50MHz v1

--- verilog/include/mm_defines.vh
+++ verilog/include/mm_defines.vh
@@ -2,8 +2,8 @@
 `define MM_DEFINES_INCLUDED
 
 
-`define MM_CLK_IN_MHZ	100
-`define MM_CLK_1S_CNT	27'h5f5e100
+`define MM_CLK_IN_MHZ	50
+`define MM_CLK_1S_CNT	(27'h5f5e100/2)
 
 `define endian_swap(a) {a[7:0], a[15:8], a[23:16], a[31:24]}
 
--- verilog/xilinx/clkgen.v
+++ verilog/xilinx/clkgen.v
@@ -48,10 +48,10 @@ ODDR2 ODDR2_inst3 (
 );
 
 DCM_CLKGEN #( // {{{
-	.CLKFXDV_DIVIDE(4), // CLKFXDV divide value (2, 4, 8, 16, 32)
+	.CLKFXDV_DIVIDE(2), // CLKFXDV divide value (2, 4, 8, 16, 32)
 	.CLKFX_DIVIDE(1), // Divide value - D - (1-256)
 	.CLKFX_MD_MAX(0.0), // Specify maximum M/D ratio for timing anlysis
-	.CLKFX_MULTIPLY(4), // Multiply value - M - (2-256)
+	.CLKFX_MULTIPLY(2), // Multiply value - M - (2-256)
 	.CLKIN_PERIOD(40), // Input clock period specified in nS
 	.SPREAD_SPECTRUM("NONE"), // Spread Spectrum mode "NONE", "CENTER_LOW_SPREAD", "CENTER_HIGH_SPREAD",
 	// "VIDEO_LINK_M0", "VIDEO_LINK_M1" or "VIDEO_LINK_M2"
